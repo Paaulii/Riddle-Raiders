2fa4f77f9de6158268b1a804754fc3c5aac43236
ZuzRad <zuzarad785@student.polsl.pl>
2023-04-17 12:27:12 +0200

    geyser jumping

---
+++ game-dev-project/Assets/Scripts/Environment/Geyser.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour
+{
+    [Header("Jump settings")]
+    public float power;
+
+    private Rigidbody2D rb;
+    private Vector2 position;
+
+
+    private void AddForce(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.name == "Small Player" || collision.name == "Big Player")
+        {
+            position = new(0, power);
+            rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.AddForce(position, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AddForce(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        AddForce(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        AddForce(collision);
+    }
+}
